fc9a66be8fdb859248e3710f95d9bb1bef63ea40
Steffen Lindner <mail@steffen-lindner.de>
2020-02-26 14:52:58 +0100

    Move mosaic out of loggedin

--- src/lakof.theme/src/lakof/theme/theme_resources/src/lakof.theme/default.js
+++ src/lakof.theme/src/lakof/theme/theme_resources/src/lakof.theme/default.js
@@ -6,11 +6,14 @@ import './theme.less';
 import jQuery from 'jquery';
 import registry from 'pat-registry';
 
+import(/* webpackChunkName: "mosaic" */ 'mosaic').then(
+    () => jQuery(($) => setTimeout(() => registry.scan(document.body)))
+  );
+
 if (jQuery('body.userrole-anonymous').length === 0) {
     import(/* webpackChunkName: "logged-in" */ './logged-in').then(
     () => jQuery(($) => setTimeout(() => registry.scan(document.body)))
   );
-  import(/* webpackChunkName: "mosaic" */ 'mosaic');
   import(/* webpackChunkName: "layouts-editor" */ 'layouts-editor');
 }
 if (jQuery('body.userrole-anonymous').length > 0) {
