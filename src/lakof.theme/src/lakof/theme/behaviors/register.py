3f1409b67d00ea6a812c69daaa264f7fcdf6b1bb
Philip Bauer <bauer@starzel.de>
2020-03-24 11:09:03 +0100

    add behavor for events to link to regostration form

---
+++ src/lakof.theme/src/lakof/theme/behaviors/register.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from plone.autoform import directives
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import model
+from zope import schema
+from plone import api
+from z3c.form.browser.radio import RadioFieldWidget
+from zope.interface import provider
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+
+
+@provider(IFormFieldProvider)
+class IRegister(model.Schema):
+
+    directives.widget(register=RadioFieldWidget)
+    register = schema.Choice(
+        title=u'Anmeldeformular',
+        vocabulary='lakof.registrationforms',
+        required=False,
+        )
+
+
+@provider(IVocabularyFactory)
+def registrationforms(context):
+    brains = api.content.find(portal_type='EasyForm', sort_on='sortable_title')
+    terms = [SimpleTerm(value='', token='', title='Kein Anmeldeformular verlinken')]
+    for brain in brains:
+        terms.append(SimpleTerm(value=brain.UID, token=brain.UID, title=brain.Title))
+    return SimpleVocabulary(terms)
